cdb2ef96492343249d8674ae5bcd77b80e517bc6
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-01-20 22:01:57 +0600

    Layout component with ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import "./App.css";
+import MainLayout from "./components/layout/MainLayout";
+
 
 function App() {
 
   return (
     <>
-      <h1>University Management App</h1>
+      <MainLayout/>
     </>
   );
 }
